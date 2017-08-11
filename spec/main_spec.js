8a1ccd7428671c756baa561d20dec3cab5e174e3
陈昱竹 <826537618@qq.com>
2017-08-11 08:27:49 +0800

    task7 finished

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -12,19 +12,32 @@ var main = require("../lib/main.js");
 describe("测试描述", function(){
     sinon.spy(console, 'log');
 
-    it("测试用例1", function(){
-
-        var result = main();
-        var expect_string = '';
-        
-        expect(expect_string).to.equal(result);
-    });
+    // it("测试用例1", function(){
+    //
+    //     var result = main(1);
+    //     var expect_string = '1 bottle of beer on the wall, 1 bottle of beer.' +
+    //         'Take one down and pass it around, no more bottles of beer on the wall.' +
+    //         'No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.';
+    //
+    //     expect(expect_string).to.equal(result);
+    // });
 
     it("测试用例2", function(){
 
-        main();
+        main(5);
         var result = _.flatten(console.log.args).join("\n");
-        var expect_string = '';
+        var expect_string = '5 bottles of beer on the wall, 5 bottles of beer.' +
+            'Take one down and pass it around, 4 bottles of beer on the wall.' +
+            '4 bottles of beer on the wall, 4 bottles of beer.' +
+            'Take one down and pass it around, 3 bottles of beer on the wall.' +
+            '3 bottles of beer on the wall, 3 bottles of beer.' +
+            'Take one down and pass it around, 2 bottles of beer on the wall.' +
+            '2 bottles of beer on the wall, 2 bottles of beer.' +
+            'Take one down and pass it around, 1 bottle of beer on the wall.' +
+            '1 bottle of beer on the wall, 1 bottle of beer.' +
+            'Take one down and pass it around, no more bottles of beer on the wall.' +
+            'No more bottles of beer on the wall, no more bottles of beer.' +
+            'Go to the store and buy some more, 99 bottles of beer on the wall.';
 
         expect(expect_string).to.equal(result);
     });
