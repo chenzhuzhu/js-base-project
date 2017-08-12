b076ad12b656c8f266708796906a54356fabea03
陈昱竹 <826537618@qq.com>
2017-08-12 22:09:53 +0800

    task7 finished & push tasking picture

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -13,28 +13,40 @@ describe("测试描述", function(){
     sinon.spy(console, 'log');
 
     // it("测试用例1", function(){
-    //
-    //     var result = main(1);
-    //     var expect_string = '1 bottle of beer on the wall, 1 bottle of beer.' +
-    //         'Take one down and pass it around, no more bottles of beer on the wall.' +
-    //         'No more bottles of beer on the wall, no more bottles of beer.Go to the store and buy some more, 99 bottles of beer on the wall.';
-    //
-    //     expect(expect_string).to.equal(result);
-    // });
 
-    it("测试用例2", function(){
+//         var result = main(1);
+//         var expect_string = `1 bottle of beer on the wall, 1 bottle of beer.
+// Take one down and pass it around, no more bottles of beer on the wall.
+// No more bottles of beer on the wall, no more bottles of beer.
+// Go to the store and buy some more, 99 bottles of beer on the wall.`;
+//
+//         expect(expect_string).to.equal(result);
+//     });
 
-        main(5);
+//     it("测试用例2", function(){
+//
+//         main(5);
+//         var result = _.flatten(console.log.args).join("\n");
+//         var expect_string = `5 bottles of beer on the wall, 5 bottles of beer.
+// Take one down and pass it around, 4 bottles of beer on the wall.
+// 4 bottles of beer on the wall, 4 bottles of beer.
+// Take one down and pass it around, 3 bottles of beer on the wall.
+// 3 bottles of beer on the wall, 3 bottles of beer.
+// Take one down and pass it around, 2 bottles of beer on the wall.
+// 2 bottles of beer on the wall, 2 bottles of beer.
+// Take one down and pass it around, 1 bottle of beer on the wall.
+// 1 bottle of beer on the wall, 1 bottle of beer.
+// Take one down and pass it around, no more bottles of beer on the wall.
+// No more bottles of beer on the wall, no more bottles of beer.
+// Go to the store and buy some more, 99 bottles of beer on the wall.`;
+//
+//         expect(expect_string).to.equal(result);
+//     });
+    it("测试用例3", function(){
+
+        main(1);
         var result = _.flatten(console.log.args).join("\n");
-        var expect_string = `5 bottles of beer on the wall, 5 bottles of beer.
-Take one down and pass it around, 4 bottles of beer on the wall.
-4 bottles of beer on the wall, 4 bottles of beer.
-Take one down and pass it around, 3 bottles of beer on the wall.
-3 bottles of beer on the wall, 3 bottles of beer.
-Take one down and pass it around, 2 bottles of beer on the wall.
-2 bottles of beer on the wall, 2 bottles of beer.
-Take one down and pass it around, 1 bottle of beer on the wall.
-1 bottle of beer on the wall, 1 bottle of beer.
+        var expect_string = `1 bottle of beer on the wall, 1 bottle of beer.
 Take one down and pass it around, no more bottles of beer on the wall.
 No more bottles of beer on the wall, no more bottles of beer.
 Go to the store and buy some more, 99 bottles of beer on the wall.`;
